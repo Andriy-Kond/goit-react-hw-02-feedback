5494681f859935ccc5c805b8b7b594abc1c066a5
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-03-19 12:28:56 +0200

    refactoring FeedbackOptions options={['good', 'neutral', 'bad']}
              <FeedbackOptions
                options={['good', 'neutral', 'bad']}
                onLeaveFeedback={this.handleClick}
              ></FeedbackOptions>

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,28 +1,22 @@
 import css from './FeedbackOptions.module.css';
 import PropTypes from 'prop-types';
-import { Component } from 'react';
 
-export class FeedbackOptions extends Component {
-  state = {
-    options: ['good', 'neutral', 'bad'],
-  };
-
-  render() {
-    return this.state.options.map(option => {
-      return (
-        <button
-          key={option}
-          className={css.buttons}
-          name={option}
-          onClick={() => this.props.onLeaveFeedback(option)}
-        >
-          {option}
-        </button>
-      );
-    });
-  }
-}
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return options.map(option => {
+    return (
+      <button
+        key={option}
+        className={css.buttons}
+        name={option}
+        onClick={() => onLeaveFeedback(option)}
+      >
+        {option}
+      </button>
+    );
+  });
+};
 
 FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
   onLeaveFeedback: PropTypes.func.isRequired,
 };
